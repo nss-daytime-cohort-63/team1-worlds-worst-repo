485391badb5e3f21f227920aab1cc19876641ddb
Alex Bishop <alexb98@gmail.com>
2023-03-01 10:55:48 -0600

    Updated footer and CSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
-import { alexFunction } from "./randomColor.js";
+import { alexFunction, goldenrod } from "./randomColor.js";
 import { HulkHogan } from "./HulkHogan.js";
 
 let alexSection = document.querySelector("#alex")
+let footer = document.querySelector("footer")
 
 const alexShuffle = () => {
     alexSection.innerHTML = alexFunction()
 }
 
+const footerShuffle = () => {
+    footer.innerHTML = goldenrod()
+}
+
 alexShuffle()
 let text = setInterval(alexShuffle, 333)
+
+footerShuffle()
+let footermove = setInterval(footerShuffle, 111)
 
 HulkHogan();
